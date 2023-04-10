d92e50a08efff966ee3f0ef7f2abbe9ba817b8bb
muhamadbaharudin64@gmail.com <muhamadbaharudin64@gmail.com>
2023-04-10 19:38:24 +0700

    praktikum03

---
+++ CODE/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pertemuan_ke3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            mahasiswa mahasiswa1 = new mahasiswa();
+            mahasiswa mahasiswa2 = new mahasiswa();
+
+            mahasiswa1.NAMA = "bahar";
+            mahasiswa1.NIM = "22.11.4935";
+            mahasiswa1.IPK = 3.50f;
+            mahasiswa2.NAMA = "sayang";
+            mahasiswa2.NIM = "22.11.4935";
+            mahasiswa2.IPK = 3.50f;
+
+
+            mahasiswa1.Registrasi();
+            mahasiswa1.IsiKRS();
+            mahasiswa2.Registrasi();
+            mahasiswa2.IsiKRS();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CODE/mahasiswa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pertemuan_ke3
+{
+    public class mahasiswa
+    {
+        // propertis
+            public string NIM { get; set; }
+            public string NAMA { get; set; }
+            public float IPK { get; set; }
+        //method
+            public void Registrasi ()
+        {
+            Console.WriteLine("NAMA: {0}", NAMA);
+            Console.WriteLine("NIM: {0}", NIM);
+            Console.WriteLine("IPK: {0}", IPK);
+            Console.WriteLine("Telah melakukan registrasi");
+        }
+
+        public void IsiKRS()
+        {
+            Console.WriteLine("{0}sedang melakukan proses KRS\n", NAMA);
+        }
+    }
+}
